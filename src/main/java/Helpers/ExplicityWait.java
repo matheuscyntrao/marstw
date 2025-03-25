68a5ac187d14161bcd71e71193804189b8f3692a
Matheus Cyntrão de Souza <matheus.cyntrao@gmail.com>
2025-03-25 01:21:26 -0300

    Adicionando o explicity wait do selenium para mapear os steps definitions.

---
+++ src/main/java/Helpers/ExplicityWait.java
@@ -0,0 +1,16 @@
+package Helpers;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
+public class ExplicityWait {
+
+    public static void waitUntilElementIsVisible(WebDriver driver, Integer seconds, By by) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(seconds));
+        wait.until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+}
