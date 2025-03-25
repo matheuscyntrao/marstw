e1a0f0cc538867720d39ce61b960a901360ba17b
Matheus Cyntrão de Souza <matheus.cyntrao@gmail.com>
2025-03-25 02:00:44 -0300

    Adicionando classe de promocode generator feita por IA.

--- src/main/java/Helpers/PromoCodeGenerator.java
+++ src/main/java/Helpers/PromoCodeGenerator.java
@@ -1,14 +1,13 @@
+package Helpers;
+
 import java.util.Random;
 
-public class PromoCodeGenerator {
+class PromoCodeGenerator {
 
-    public static String generateValidPromoCode() {
+    public static String generateValidPromoCode(int _discount) {
+
         Random random = new Random();
         StringBuilder code = new StringBuilder();
-
-        // First digit: Discount percentage (2-9)
-        int discount = random.nextInt(8) + 2;
-        code.append(discount);
 
         // Next two digits: Random characters
         for (int i = 0; i < 2; i++) {
@@ -29,14 +28,11 @@ public class PromoCodeGenerator {
         code.append(num2);
 
         // Calculate and append check digit
-        int checkDigit = (discount + num1 + num2) % 10;
+        int checkDigit = (_discount + num1 + num2) % 10;
         code.append(checkDigit);
 
         return code.toString();
+
     }
 
-    public static void main(String[] args) {
-        String validCode = generateValidPromoCode();
-        System.out.println("Generated Valid Promo Code: " + validCode);
-    }
 }
